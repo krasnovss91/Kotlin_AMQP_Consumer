5bfb106fd4230ee4fd65086cb38223c07239be27
krasnovss91 <krasnovstanislav91@gmail.com>
2022-06-08 09:51:58 +0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+import com.rabbitmq.client.CancelCallback
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.DeliverCallback
+import com.rabbitmq.client.Delivery
+import java.nio.charset.StandardCharsets
+
+fun main(args: Array<String>) {
+    val factory = ConnectionFactory()
+    val connection = factory.newConnection("amqp://guest:guest@localhost:15672/")
+    val channel = connection.createChannel()
+    val consumerTag = "SimpleConsumer"
+
+    channel.queueDeclare("test_queue", false, false, false, null)
+
+    println("[$consumerTag] Waiting for messages...")
+    val deliverCallback = DeliverCallback { consumerTag: String?, delivery: Delivery ->
+        val message = String(delivery.body, StandardCharsets.UTF_8)
+        println("[$consumerTag] Received message: '$message'")
+    }
+    val cancelCallback = CancelCallback { consumerTag: String? ->
+        println("[$consumerTag] was canceled")
+    }
+
+    channel.basicConsume("DEMO", true, consumerTag, deliverCallback, cancelCallback)
+}
